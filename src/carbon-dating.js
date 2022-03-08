5b84460849983d006412c046e581e0c9e86d4d73
maiklshetinin <sh_chetinin@mail.ru>
2022-03-08 15:45:19 +0300

    feat: add resolve hanoi-tower

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
+function dateSample(inp) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+//  if (isNaN(+inp) || +inp <= 0 || typeof inp !== "string" || inp.length === 0) {return false};
+//     return Math.ceil(Math.log(MODERN_ACTIVITY/inp)/(0.693/HALF_LIFE_PERIOD));
+  
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
+ function getSeason(date) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+//   let month =date.getMonth()
+//   let year =date.getFullYear()
+
+//  if(month===0||month===1||month===11)return console.log('winter')
+//   else if(month===2||month===3||month===4)return console.log('spring')
+//   else if(month===5||month===6||month===7)return console.log('summer')
+//   else if(month===8||month===9||month===10)return console.log('autumn')
 }
 
 module.exports = {
